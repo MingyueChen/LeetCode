c1fbdd51b7fc91c44e32d197fc5b4f905cf53015
MingyueChen <mingyuehappy@gmail.com>
2026-09-22 23:29:29 -0400

    Feat: add contains duplicate

---
+++ hashmap_array/contains_duplicate.js
@@ -0,0 +1,30 @@
+/**
+Given an integer array nums, return true if any value appears at least twice in the array, 
+and return false if every element is distinct.
+
+ 
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+*/
+
+function containsDuplicate(nums) {
+  const set = new Set();
+  for (const num of nums) {
+    if (set.has(num)) {
+      return true;
+    }
+    set.add(num);
+  }
+  return false;
+}
